42f30aa87a23739b7d2f3dc0a9ccf122f37ddfc5
Seb <linoshizu@gmail.com>
2025-03-31 22:33:50 -0300

    Configures TS

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
+});
