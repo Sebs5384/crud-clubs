43de1fd82f74326d00e31d635105de9adb4021b2
Seb <linoshizu@gmail.com>
2025-04-03 17:18:51 -0300

    Setup cors in the backend
    This will account for CORS issue when trying to fetch data from the frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
 import teamsRouter from './routes/teamsRoutes';
 import teamRouter from './routes/teamRoutes';
+import cors from "cors";
 import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use("/api", teamsRouter);
 app.use("/api", teamRouter);
 
